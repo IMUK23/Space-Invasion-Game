e074a4e462f5816b8cb64405705729ebcbfe84b6
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-07 18:20:04 +0530

    Update ship.py

--- ship.py
+++ ship.py
@@ -7,6 +7,6 @@ class Ship():
         self.screen_rect=screen.get_rect()
         self.rect.centerx=self.screen_rect.centerx
         self.rect.bottom=self.screen_rect.bottom
-        def blitme(self):
-            self.screen.blit(self.image,self.rect)
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
         
