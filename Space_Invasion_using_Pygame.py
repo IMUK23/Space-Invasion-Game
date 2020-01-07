8575cf1f71f67d22da87b285bc411051c59c35c9
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-07 15:53:44 +0530

    Add files via upload

---
+++ Space_Invasion_using_Pygame.py
@@ -0,0 +1,5 @@
+
+content="Hey this is the readme document of the game Space Invasion \n This is my first project in python \n \n The tools and other things used in production of this game are listed below: \n 1)Visual Studio as editor \n 2) Python language(pygame library)"
+with open('readme.txt','w') as file:
+    file.write(content)
+
