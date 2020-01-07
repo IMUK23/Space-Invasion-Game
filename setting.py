fea4ddbc333f5db4bf1a276051c5872a3f07d297
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-07 20:47:02 +0530

    Update setting.py

--- setting.py
+++ setting.py
@@ -4,4 +4,10 @@ class Settings():
         self.width=1200
         self.height=800
         self.bgcolor=(230,230,230)
-
+         self.moving_right=False
+        self.moving_left=False
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
