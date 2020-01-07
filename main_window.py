3cb7a9ed134b23e6d316e36021ff40ae2290cbdf
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-07 16:06:36 +0530

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -3,11 +3,12 @@ def run_game():
     pygame.init()
     screen=pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Space Invasion")
-
+    bgcolor=(230,230,230)
     while True:
         for event in pygame.event.get():
-            if event.type== pygame.QUIT():
+            if event.type== pygame.QUIT:
                 sys.exit()
+        screen.fill(bgcolor)        
         pygame.display.flip()
 run_game()
 
