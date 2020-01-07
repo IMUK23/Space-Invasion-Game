151721646e11edc4ca3245e1918592952f87a0fc
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-07 15:57:53 +0530

    Add files via upload

---
+++ main_window.py
@@ -0,0 +1,13 @@
+import sys,pygame
+def run_game():
+    pygame.init()
+    screen=pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Space Invasion")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type== pygame.QUIT():
+                sys.exit()
+        pygame.display.flip()
+run_game()
+
