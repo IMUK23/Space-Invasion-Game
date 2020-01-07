ef95abe02dcabd2af811305c409f98010f184f89
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-07 18:21:17 +0530

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -1,16 +1,19 @@
 import sys,pygame
 from setting import Settings
+from ship import Ship
 def run_game():
     pygame.init()
     ##ai_setting is an object of Setting class
     ai_settings=Settings()
     screen=pygame.display.set_mode((ai_settings.width,ai_settings.height))
     pygame.display.set_caption("Space Invasion")
+    ship=Ship(screen)
     while True:
         for event in pygame.event.get():
             if event.type== pygame.QUIT:
                 sys.exit()
         screen.fill(ai_settings.bgcolor)        
+        ship.blitme()
         pygame.display.flip()
 run_game()
 
