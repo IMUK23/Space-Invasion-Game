8e6050212d2d81993419f051ed962c17e3d0c1a3
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-14 20:54:46 +0530

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,12 +2,11 @@
 class Settings():
     def __init__(self):
         self.width=1200
-        self.height=800
+        self.height=700
         self.bgcolor=(230,230,230)
-         self.moving_right=False
-        self.moving_left=False
-    def update(self):
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        self.ship_shift_factor=1.5
+        self.bullets_speed_factor=1
+        self.bullets_width=15
+        self.bullets_height=3
+        self.bullets_color=60,60,60
+        self.bullets_allowed=3
