927314e68345b2793800a0cf17ebce6bb46feb3e
Utkarsh Bahukhandi <54958021+IMUK23@users.noreply.github.com>
2020-01-14 20:53:46 +0530

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -2,17 +2,21 @@ import sys,pygame
 from setting import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group 
+from bullets import Bullet
 def run_game():
     pygame.init()
-    ##ai_setting is an object of Setting class
     ai_settings=Settings()
     screen=pygame.display.set_mode((ai_settings.width,ai_settings.height))
     pygame.display.set_caption("Space Invasion")
-    ship=Ship(screen)
+    ship=Ship(ai_settings,screen)
+    bullets=Group()
     while True:
-        gf.check_events(ship)
-        screen.fill(ai_settings.bgcolor)        
-        ship.blitme()
+        gf.check_event(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         pygame.display.flip()
 run_game()
 
+    
